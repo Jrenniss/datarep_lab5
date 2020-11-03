6286c277f0d92da03e7a9b0e24d40b90e764bb90
Jrenniss <jrenniss@gmail.com>
2020-11-03 15:36:42 +0000

    Q4 - New Root & Req/Res

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//Creating Roots
+//Creating Roots - http://localhost:3000
 app.get('/', (req, res) => {
   res.send('Welcome to Data Representation and Querying!')
+})
+
+//New Root Path - http://localhost:3000/hello
+//Basic API - http request and response using names
+app.get('/hello/:name',(req, res)=>{
+    console.log(req.params.name);
+    res.send('Hello ' + req.params.name);
 })
 
 
